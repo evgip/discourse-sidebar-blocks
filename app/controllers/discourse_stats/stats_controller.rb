f7413e54971cfc4aaca758b6d10d95ec8191b127
Evg <budo@narod.ru>
2017-07-01 00:00:35 +0300

    Update stats_controller.rb

--- app/controllers/discourse_stats/stats_controller.rb
+++ app/controllers/discourse_stats/stats_controller.rb
@@ -2,7 +2,7 @@ module DiscourseStats
 	class StatsController < ApplicationController
 
 		def my_page
-			render :json => { name: "Нас посетили за 24 часа..." }
+			render :json => { name: "Нас посетили за 24 часа... (в стадии разработки)" }
       rescue StandardError => e
         render_json_error e.message
    
