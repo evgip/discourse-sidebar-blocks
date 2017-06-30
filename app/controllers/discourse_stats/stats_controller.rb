973a241072ceb52888d90170f5f2f5c36840668b
Evg <budo@narod.ru>
2017-06-30 23:56:55 +0400

    Update stats_controller.rb

--- app/controllers/discourse_stats/stats_controller.rb
+++ app/controllers/discourse_stats/stats_controller.rb
@@ -2,7 +2,7 @@ module DiscourseStats
 	class StatsController < ApplicationController
 
 		def my_page
-			render :json => { name: "succotash", description: "yellow and green" }
+			render :json => { name: "Нас посетили за 24 часа..." }
       rescue StandardError => e
         render_json_error e.message
    
