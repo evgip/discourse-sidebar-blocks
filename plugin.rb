efb3c8fb05986dcf1cf9fae3acf358fdd8ff234c
Evg <budo@narod.ru>
2017-07-08 22:29:10 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -12,3 +12,24 @@ Discourse::Application.routes.append do
 end
 
 load File.expand_path('../lib/stats/engine.rb', __FILE__)
+
+after_initialize do
+
+  require 'listable_topic_serializer'
+  class ::ListableTopicSerializer
+
+    def excerpt
+      accepted_id = object.custom_fields["accepted_answer_post_id"].to_i
+      if accepted_id > 0
+        cooked = Post.where(id: accepted_id).pluck('cooked')
+        PrettyText.excerpt(cooked[0], 200, {})
+      else
+        object.excerpt
+      end
+    end
+
+    def include_excerpt?
+      true
+    end
+  end
+end
